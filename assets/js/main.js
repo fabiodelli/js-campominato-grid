fe933a0e971c1f3d7285c9ee82449df8d986cdca
fabiodelli <ntqdel@hotmail.com>
2023-03-17 17:20:47 +0100

    add function generateGrid

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,11 @@ const containerEl = document.querySelector('.container')
 const cellEl = document.querySelectorAll('.cell')
 let cellMax = 64
 
-
+function generateGrid(){
 for (let i = 0; i < cellMax; i++) {
   let numbercell = i+1
   const cell = `<div class="cell">${numbercell}</div>`;
   containerEl.innerHTML += cell;
-}
+}}
+
 
