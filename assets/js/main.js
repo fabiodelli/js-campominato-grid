fbe8bf9e7aa3fec14651d8ccb3ae708f8ea2fd53
fabiodelli <ntqdel@hotmail.com>
2023-03-17 22:10:17 +0100

    add greenOnClick

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,27 @@
 
 const containerEl = document.querySelector('.container')
-const cellEl = document.querySelectorAll('.cell')
-console.log(cellEl);
+
 let cellMax = 100
 
 function generateGrid(){
 for (let i = 0; i < cellMax; i++) {
-  let numbercell = i+1
-  const cell = `<div onclick="greenOnClick()" class="cell">${numbercell}</div>`;
+  let numberCell = i+1
+  const cell = `<div onclick="greenOnClick()" class="cell">${numberCell}</div>`;
   containerEl.innerHTML += cell;
 }}
-function greenOnClick(){
-for (let i = 0; i < cellEl.length; i++) {
-  const thisCell = cellEl[i];
-  console.log(thisCell)
-  thisCell.addEventListener("click", function() {
-      thisCell.classList.toggle("bg_green")
-      // this.classList.add("active")
-      console.log("Changed the color")
-  })
-}}
 
+
+ 
+
+  function greenOnClick() {
+    const cellEl = document.querySelectorAll('.cell')
+
+    for (let i = 0; i < cellEl.length; i++) {
+      const thisCell = cellEl[i];
+      
+          thisCell.classList.toggle("bg_green")
+          // this.classList.add("active")
+          console.log("Changed the color")
+      
+
+  }}
